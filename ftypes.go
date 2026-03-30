90a5ee966e01d3fc8807a5b39e4a628216449320
AsciiMoth <ascii@moth.contact>
2026-03-30 23:43:13 +0400

    feat: add common func types

---
+++ ftypes.go
@@ -0,0 +1,79 @@
+package gonnect
+
+import (
+	"context"
+	"net"
+	"net/netip"
+)
+
+// Static type assertions
+var (
+	_ Dial         = (&net.Dialer{}).DialContext
+	_ Listen       = (&net.ListenConfig{}).Listen
+	_ LookupIP     = (&net.Resolver{}).LookupIP
+	_ LookupIPAddr = (&net.Resolver{}).LookupIPAddr
+	_ LookupNetIP  = (&net.Resolver{}).LookupNetIP
+	_ LookupHost   = (&net.Resolver{}).LookupHost
+	_ LookupAddr   = (&net.Resolver{}).LookupAddr
+	_ LookupCNAME  = (&net.Resolver{}).LookupCNAME
+	_ LookupPort   = (&net.Resolver{}).LookupPort
+	_ LookupTXT    = (&net.Resolver{}).LookupTXT
+	_ LookupMX     = (&net.Resolver{}).LookupMX
+	_ LookupNS     = (&net.Resolver{}).LookupNS
+	_ LookupSRV    = (&net.Resolver{}).LookupSRV
+)
+
+// PacketConn is an interface for UDP-like packet connections.
+type PacketConn interface {
+	net.PacketConn
+	net.Conn
+}
+
+// Dialer is a function type for establishing TCP-like connections.
+// It matches the signature of net.Dialer.DialContext.
+type Dial = func(ctx context.Context, network, address string) (net.Conn, error)
+
+// PacketDialer is a function type for establishing UDP-like packet connections.
+type PacketDial = func(ctx context.Context, network, address string) (PacketConn, error)
+
+// Listener is a function type for creating TCP-like listeners.
+type Listen = func(ctx context.Context, network, address string) (net.Listener, error)
+
+// PacketListener is a function type for creating UDP-like packet listeners.
+type PacketListen = func(ctx context.Context, network, address string) (PacketConn, error)
+
+// LookupIP looks up host. It returns a slice of that host's IPv4 and IPv6 addresses.
+type LookupIP = func(ctx context.Context, network, address string) ([]net.IP, error)
+
+// LookupIPAddr looks up host just like LookupIP but unlike it returns slice of net.IPAddr.
+type LookupIPAddr = func(ctx context.Context, host string) ([]net.IPAddr, error)
+
+// LookupNetIP looks up host just like LookupIP but unlike it returns slice of netip.Addr.
+type LookupNetIP = func(ctx context.Context, network, host string) ([]netip.Addr, error)
+
+// LookupHost looks up host just like LookupIP but unlike it returns slice of IP strings.
+type LookupHost = func(ctx context.Context, host string) (addrs []string, err error)
+
+// LookupAddr performs a reverse lookup for the given address,
+// returning a list of names mapping to that address.
+type LookupAddr = func(ctx context.Context, addr string) (names []string, err error)
+
+// LookupCNAME returns the canonical name for the given host.
+type LookupCNAME = func(ctx context.Context, host string) (cname string, err error)
+
+// LookupPort looks up the port for the given network and service.
+type LookupPort = func(ctx context.Context, network, service string) (port int, err error)
+
+// LookupTXT returns the DNS TXT records for the given domain name.
+// If a DNS TXT record holds multiple strings, they are concatenated as a single string.
+type LookupTXT = func(ctx context.Context, name string) ([]string, error)
+
+// LookupMX returns the DNS MX records for the given domain name sorted by preference.
+type LookupMX = func(ctx context.Context, name string) ([]*net.MX, error)
+
+// LookupNS returns the DNS NS records for the given domain name.
+type LookupNS = func(ctx context.Context, name string) ([]*net.NS, error)
+
+// LookupSRV tries to resolve an SRV query of the given service, protocol, and domain name.
+// The proto is "tcp" or "udp".
+type LookupSRV = func(ctx context.Context, service, proto, name string) (string, []*net.SRV, error)

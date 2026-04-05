d3a8304ef6aeea3f5fc51924da356ae1887270e7
AsciiMoth <ascii@moth.contact>
2026-04-05 21:57:17 +0400

    feat: add special handling for tls.Conn in GetWrapped

--- wrapper.go
+++ wrapper.go
@@ -1,9 +1,16 @@
 package gonnect
 
+import "net"
+
 // Wrapper is an interface for types that wrap underlying values.
 // Implementing types should return the wrapped value via GetWrapped.
 type Wrapper interface {
 	GetWrapped() any
+}
+
+// withNetConn is implemented by tls.Conn
+type withNetConn interface {
+	NetConn() net.Conn
 }
 
 // GetWrapped extracts the wrapped value from an object.
@@ -14,6 +21,10 @@ func GetWrapped(obj any) any {
 	}
 	if wr, ok := obj.(Wrapper); ok {
 		return wr.GetWrapped()
+	}
+	// Special case for tls.Conn
+	if wn, ok := obj.(withNetConn); ok {
+		return wn.NetConn()
 	}
 	return nil
 }
